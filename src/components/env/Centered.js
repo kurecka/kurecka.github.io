96fc7f8aea2be6dbf21856e4be66c1125bf872f6
Martin Kurečka <kur3cka.martin@gmail.com>
2022-06-27 19:34:40 +0200

    Updates

--- src/components/env/Centered.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-export default function Centered({children}) {
-    return (
-        <div className="mx-auto px-10 max-w-screen-md">
-        {children}
-        </div>
-    )
-}
